7ea7637971a98582955fde585c17934377bc1c05
ShreeRaz <ankitt60raz@gmail.com>
2026-05-11 23:32:09 +0545

    Added mapping and supabase

--- app/report.tsx
+++ app/report.tsx
@@ -1,9 +1,54 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
 
 export default function ReportScreen() {
+  const [description, setDescription] = useState("");
   return (
-    <View>
-      <Text>This is report section of the page</Text>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        value={description}
+        onChangeText={setDescription}
+        placeholder="Describe the location..."
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => console.log("Alert tapped!")}
+      >
+        <Text style={styles.buttonText}>🐘 Send Alert</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    padding: 12,
+    fontSize: 16,
+    color: "white",
+  },
+  button: {
+    backgroundColor: "#f97316",
+    padding: 16,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 16,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
